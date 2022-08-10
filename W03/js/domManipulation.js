505e0f1d838ea9b56845603bca0840b00d758861
Max Bye <mbye@techsoup.org>
2022-08-10 19:51:00 +0100

    simple style exercises

---
+++ W03/js/domManipulation.js
@@ -0,0 +1,4 @@
+    document.getElementById('helloWorldHeader').style.color = 'red';
+    for(let i = 0; i < document.getElementsByTagName('li').length; i++){
+      document.getElementsByTagName('li')[i].style.color = 'green';
+    }
