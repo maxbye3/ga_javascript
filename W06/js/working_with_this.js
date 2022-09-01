1072198c84b4465604349797f10ae15ac47f059a
Max Bye <maxbye@gmail.com>
2022-09-01 10:33:55 +0100

    this explanation

---
+++ W06/js/working_with_this.js
@@ -0,0 +1,54 @@
+// /*
+// * this
+// */
+// const pig = {
+//     firstName: 'hog',
+//     lastName: 'zilla',
+//     type: 'texas feral',
+//     getPig: function () {
+//         return this.firstName + this.lastName;
+//     }
+// }
+
+// const details = {
+//     firstName: 'Max',
+//     lastName: 'Bye',
+//     fullName: function () {
+//         // details
+//         return this.firstName + this.lastName;
+//     }
+// }
+
+// // this === window 
+// console.log(`first name: ${pig.firstName}`);
+// console.log(`last name: ${pig.lastName}`);
+// console.log(`full name: ${pig.getPig()}`);
+// console.log(`full name: ${details.fullName()}`);
+
+let data = [];
+
+const getBlog = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => {
+            return result.json();
+        }).then((getBlogs) => {
+            data = getBlogs;
+        }).catch((error) => {
+            console.log('error:', error);
+        });
+}
+
+console.log(data);
+
+// eslint-disable-next-line no-unused-vars
+const filterData = () => {
+    // let helloWorld = 'hello world';
+
+    let filteredResult = data.filter((blog) => {
+        return blog.id === 100;
+    });
+    console.log(filteredResult);
+}
+
+
+getBlog();
